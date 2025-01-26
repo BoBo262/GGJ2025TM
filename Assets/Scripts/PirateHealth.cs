31e4b7eddb9d49b865d4638240e0d275ead8501a
BoBo262 <79087084+BoBo262@users.noreply.github.com>
2025-01-26 15:31:17 +0200

    repaired pirate

---
+++ Assets/Scripts/PirateHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateHealth : MonoBehaviour
+{
+    DamageSink damagePirate;
+    [SerializeField] public float health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        damagePirate = gameObject.GetComponent<DamageSink>();
+        damagePirate.hp = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<BulletBehavior>())
+        {
+            BulletBehavior bubble = collision.gameObject.GetComponent<BulletBehavior>();
+            damagePirate.TakeDamage(bubble.damage);
+            Destroy(bubble.gameObject);
+            if (damagePirate.destroyed)
+            {
+                OnDestroy();
+            }
+        }
+    }
+
+    public void OnDestroy()
+    {
+        Debug.Log("Pirate Killed");
+        Destroy(gameObject);
+
+    }
+}
